2bac13a8fc75c08864d4ddb7355e3748db97fa45
FoxSide <foxside8452@gmail.com>
2022-03-07 21:31:38 +0200

    16

--- src/utils/errorUtils.ts
+++
@@ -1,9 +0,0 @@
-import {ActionsType, setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
-import {Dispatch} from "redux";
-import {addTodolistAC} from "../features/TodolistsList/todolists-reducer";
-
-export const handleServerNetworkError = (dispatch: Dispatch<ActionsType>, message: string) => {
-  dispatch(setAppErrorAC(message))
-  dispatch(setAppStatusAC('succeeded'))
-}
-
